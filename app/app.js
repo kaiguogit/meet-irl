24af8ea631a5d0954f7a7c77dafb5b9aa3795d63
Matt Larson <matt.a.larson@gmail.com>
2017-09-17 14:44:34 -0500

    almost on DOM, but ng error

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
-angular.module('myApp', [
-  'ui.router'
-])
-.config(($urlRouterProvider) => {
-  $urlRouterProvider.otherwise("/")
-})
+(function() {
+  'use strict';
+
+  angular.module('meetIrl', [
+    'ui.router',
+    'api.users',
+    'components.users'
+  ])
+  .config(function($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/users');
+  });
+})();
